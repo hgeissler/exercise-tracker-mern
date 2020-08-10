6eb501bc4859345b9bcaf187b9cdba4ba4fa752d
Geißler Hannes <h.geissler@pke-de.com>
2020-08-10 09:45:03 +0200

    init server config

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.ATLAS_URI
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+})
+
+const connection = mongoose.connection
+connection.on('error', console.error.bind(console, 'connection error:'))
+connection.once('open', () => {
+  console.log('MongoDB database connection established sucessfully')
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on Port ${port}`)
+})
