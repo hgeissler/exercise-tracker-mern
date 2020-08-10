a73e1c6c7b3edfb058c5c0b11d6c5007bf0655e7
Geißler Hannes <h.geissler@pke-de.com>
2020-08-10 12:28:00 +0200

    added routes for users and exercises

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,12 @@ connection.once('open', () => {
   console.log('MongoDB database connection established sucessfully')
 })
 
+const exercisesRouter = require('./routes/exercises')
+const usersRouter = require('./routes/users')
+
+app.use('/exercises', exercisesRouter)
+app.use('/users', usersRouter)
+
 app.listen(port, () => {
   console.log(`Server is listening on Port ${port}`)
 })
